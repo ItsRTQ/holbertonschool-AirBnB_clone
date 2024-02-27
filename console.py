46db0280ca159fe95415e85e51979be6bdaf75c1
Abdiel-88 <abdiel.f@outlook.com>
2024-02-26 22:20:13 -0400

    forgot to add dockstring

--- console.py
+++ console.py
@@ -1,19 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+This module defines the HBNBCommand class.
+It serves as the entry point for the command interpreter for the HBnB project.
+"""
+
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    """Command interpreter for the HBnB project."""
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        'Quit command to exit the program'
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program"""
+        'EOF command to exit the program'
         return True
 
     def emptyline(self):
-        """An empty line + ENTER shouldn’t execute anything"""
+        """Do nothing upon receiving an empty line."""
         pass
 
 if __name__ == '__main__':
