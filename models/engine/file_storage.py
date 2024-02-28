ccd63315261b61fffe68b28add990b663a9e1b72
Abdiel-88 <abdiel.f@outlook.com>
2024-02-27 23:09:50 -0400

    added classes to file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,56 +4,48 @@ import json
 import os
 from models.base_model import BaseModel
 
+
 class FileStorage:
     """This class serializes instances to a JSON file and deserializes"""
-
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
-        """all, returns the objects saved in storage"""
+    @staticmethod
+    def classes():
+        """Returns a dictionary of all valid classes"""
+        return {
+            "BaseModel": BaseModel,
+            # Add other model classes here
+            # "User": User,
+            # "Place": Place,
+            # etc.
+        }
 
+    def all(self):
+        """Returns the objects saved in storage"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """new, add obj to the instance attribute object
-            Adds a new instance to storage dict
-        """
+        """Adds a new instance to storage dictionary"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
 
-        temp = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[temp] = obj
- 
     def save(self):
-        """This method serialize object to json file
-            Save the data into a json file in a dict of dicts format
-        """
-        prep_data = {}
-        for key, instans in FileStorage.__objects.items():
-            if not isinstance(FileStorage.__objects[key], dict):
-                prep_data[key] = FileStorage.__objects[key].to_dict()
-            else:
-                prep_data[key] = FileStorage.__objects[key]
+        """Serializes objects to the JSON file"""
+        prep_data = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
         with open(self.__file_path, 'w') as file:
             json.dump(prep_data, file, indent=2)
 
     def reload(self):
-        """This method de-serialize object back to instance
-            Verify if the file exist 
-                if it exist it retrive the data into storage
-        """
-
+        """Deserializes the JSON file to objects"""
         if os.path.exists(self.__file_path):
             with open(self.__file_path, 'r') as file:
-                FileStorage.__objects = json.load(file)
-            for outter_dict, inner_dict in FileStorage.__objects.items():
-                key_name, obj_id = outter_dict.split('.')
-                instance_name = globals()[key_name]
-                obj = instance_name(**inner_dict)
-                FileStorage.__objects[outter_dict] = obj
-        else:
-            FileStorage.__objects = {}
+                data = json.load(file)
+                for obj_id, obj_data in data.items():
+                    class_name = obj_data['__class__']
+                    if class_name in FileStorage.classes():
+                        self.new(FileStorage.classes()[class_name](**obj_data))
 
     def reset_filestorage(self):
-        """resets file storage"""
-
+        """Resets file storage"""
         FileStorage.__objects = {}
