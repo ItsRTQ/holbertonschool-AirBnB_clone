d60ca0a1bf1d9d2fd6b85e35c3743c099d266425
William <titanbabrisa@gmail.com>
2024-02-27 21:39:33 -0400

    improve test cases

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -1,59 +1,78 @@
 import unittest
 import os
-from models.engine.file_storage import FileStorage
+import json
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
 
     def setUp(self):
-        # Create an instance of FileStorage for each test case
+
+        self.file_path = "test_file.json"
         self.file_storage = FileStorage()
-
-    def tearDown(self):
-        # Remove the file.json created during the tests
-        if os.path.exists(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
-
-    def test_all_method(self):
-        # Test if all method returns the correct dictionary
-        obj1 = BaseModel()
-        obj2 = BaseModel()
-
-        self.file_storage.new(obj1)
-        self.file_storage.new(obj2)
-
-        result = self.file_storage.all()
-        expected_result = {
-            'BaseModel.{}'.format(obj1.id): obj1.to_dict(),
-            'BaseModel.{}'.format(obj2.id): obj2.to_dict()
-        }
-
-        self.assertEqual(result, expected_result)
-
-    def test_new_method(self):
-        # Test if new method adds the object to the dictionary
-        obj = BaseModel()
-        self.file_storage.new(obj)
-
-        result = self.file_storage.all()
-        expected_result = {'BaseModel.{}'.format(obj.id): obj.to_dict()}
-
-        self.assertEqual(result, expected_result)
-
-    def test_save_and_reload_methods(self):
-        # Test if save and reload methods work correctly
-        obj = BaseModel()
-        self.file_storage.new(obj)
+        self.obj = BaseModel()
+        self.obj.id = "test_id"
+        self.obj_name = "{}.{}".format(self.obj.__class__.__name__, self.obj.id)
+        self.file_storage.new(self.obj)
         self.file_storage.save()
 
-        # Create a new instance of FileStorage to reload the data
-        new_file_storage = FileStorage()
-        new_file_storage.reload()
+    def tearDown(self):
 
-        result = new_file_storage.all()
-        expected_result = {'BaseModel.{}'.format(obj.id): obj.to_dict()}
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)
 
-        self.assertEqual(result, expected_result)
+    def test_file_path_default_value(self):
+        self.assertEqual(FileStorage._FileStorage__file_path, "file.json")
+
+    def test_objects_default_value(self):
+        self.assertEqual(FileStorage._FileStorage__objects, {})
+
+    def test_all_method(self):
+        objects = self.file_storage.all()
+        self.assertEqual(objects, {self.obj_name: self.obj})
+
+    def test_new_method(self):
+        new_obj = BaseModel()
+        new_obj.id = "new_test_id"
+        new_obj_name = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
+        self.file_storage.new(new_obj)
+        objects = self.file_storage.all()
+        self.assertEqual(objects, {self.obj_name: self.obj, new_obj_name: new_obj})
+
+    def test_save_method(self):
+        self.assertTrue(os.path.exists(self.file_path))
+        with open(self.file_path, 'r') as file:
+            data = json.load(file)
+            self.assertIn(self.obj_name, data)
+            self.assertEqual(data[self.obj_name], self.obj.to_dict())
+
+    def test_reload_method(self):
+
+        new_obj = BaseModel()
+        new_obj.id = "new_test_id"
+        new_obj_name = "{}.{}".format(new_obj.__class__.__name__, new_obj.id)
+        self.file_storage.new(new_obj)
+        self.file_storage.save()
+
+        self.file_storage._FileStorage__objects = {}
+
+        self.file_storage.reload()
+
+        objects = self.file_storage.all()
+        expected_objects = {self.obj_name: self.obj, new_obj_name: new_obj}
+        self.assertEqual(objects, expected_objects)
+
+class TestBaseModel(unittest.TestCase):
+
+    def test_save_method(self):
+        obj = BaseModel()
+        obj.save()
+        self.assertTrue(os.path.exists("file.json"))
+        with open("file.json", 'r') as file:
+            data = json.load(file)
+            key_name = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.assertIn(key_name, data)
+            self.assertEqual(data[key_name], obj.to_dict())
 
 if __name__ == '__main__':
     unittest.main()
