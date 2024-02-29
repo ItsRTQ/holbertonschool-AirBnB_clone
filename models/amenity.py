46dc61e96db8e74eb4babb38481536e6b9772b77
William <titanbabrisa@gmail.com>
2024-02-28 21:51:13 -0400

    task 9

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines class Amenity"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """This class inherits From BaseModel"""
+
+    name = ""
